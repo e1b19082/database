76603ac303f73b6d8975f5a497676d8529335e39
okaki713 <e1b19014@oit.ac.jp>
2021-10-19 14:34:41 +0900

    認証処理の実装

---
+++ src/main/java/oit/is/z0209/db/database/security/Sample3AuthConfiguration.java
@@ -0,0 +1,39 @@
+package oit.is.z0209.db.database.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class Sample3AuthConfiguration extends WebSecurityConfigurerAdapter {
+
+    /**
+     * 認証処理に関する設定（誰がログインできるか）
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+  
+      // 平文のパスワードをエンコーダにかけてハッシュ化し，"user1"と関連付けている．ロール名は"USER"
+      // プログラム中に素のパスワードが含まれることになるので望ましくない
+      auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("p@ss")).roles("USER");
+  
+      // $ sshrun htpasswd -nbBC 10 admin adm1n
+      // htpasswdでBCryptエンコードを行った後の文字列をパスワードとして指定している．
+      auth.inMemoryAuthentication().withUser("admin")
+          .password("$2y$10$3e7Hs2QZ/p91yJVgP5y/1OC7AC8jfc6YDYDzMGK1XieDlNR2nBGDe").roles("ADMIN");
+    }
+  
+    @Bean
+    PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder();
+    }
+  
+
+  }
+  
